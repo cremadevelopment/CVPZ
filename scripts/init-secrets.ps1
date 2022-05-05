0c2fb8e317ac409d8180c5339746de2abf6c6732
Carter Barnes <carter.barnes@cremadevelopment.com>
2022-05-04 20:47:19 -0700

    Create db context #887

--- scripts/init-secrets.ps1
+++ scripts/init-secrets.ps1
@@ -6,3 +6,10 @@ param (
 $userSecretsId = "e95811e3-398f-4274-a092-8f96818d2e76"
 
 dotnet user-secrets set UserConfig:YourName "$YourName" --id "$userSecretsId"
+
+$DataSource = '.,6433'
+$DatabaseName = 'CVPZ'
+$UserId = 'sa'
+$Password = 'yourStrong(!)Password'
+
+dotnet user-secrets set ConnectionStrings:CVPZ "Data Source=${DataSource};Initial Catalog=${DatabaseName};User Id=${UserId};Password=${Password}" --id "$userSecretsId"
