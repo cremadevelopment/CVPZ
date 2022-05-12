c5ba54fd77c4825ce5eb2c0530de9362923fe40d
Carter Barnes <carter.barnes@cremadevelopment.com>
2022-05-11 21:01:52 -0700

    Add command and update entity to end a job #912 #913

---
+++ src/CVPZ.Application/Job/Commands/EndJob/EndJobHandler.cs
@@ -0,0 +1,39 @@
+﻿using CVPZ.Infrastructure.Data;
+using MediatR;
+using Serilog;
+
+namespace CVPZ.Application.Job.Commands.EndJob;
+
+public class EndJobHandler : IRequestHandler<EndJobRequest, EndJobResponse>
+{
+    private readonly CVPZContext _context;
+    private readonly ILogger _logger;
+
+    public EndJobHandler(CVPZContext context, ILogger logger)
+    {
+        this._context = context;
+        this._logger = logger;
+    }
+
+    public async Task<EndJobResponse> Handle(EndJobRequest request, CancellationToken cancellationToken)
+    {
+        var validId = int.TryParse(request.JobId, out int jobId);
+        if (!validId)
+        {
+            _logger.Error("Job id of {jobId} was invalid.", request.JobId);
+            throw new KeyNotFoundException($"Job id of {request.JobId} was invalid.");
+        }
+
+        var entity = await _context.Jobs.FindAsync(jobId);
+        if (entity == null)
+        {
+            _logger.Error("Job with id {jobId} not found.", jobId);
+            throw new KeyNotFoundException($"Job with id {jobId} not found.");
+        }
+
+        entity.EndDate = request.EndDate;
+        await _context.SaveChangesAsync();
+
+        return new EndJobResponse(jobId.ToString());
+    }
+}
---
+++ src/CVPZ/Api/JobController.cs
@@ -0,0 +1,25 @@
+﻿using CVPZ.Application.Job.Commands.CreateJob;
+using CVPZ.Application.Job.Commands.EndJob;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVPZ.Api;
+
+public class JobController : BaseApiController
+{
+    public JobController(IMediator mediator) : base(mediator) { }
+
+    [HttpPost]
+    public async Task<CreateJobResponse> Post(CreateJobRequest request)
+    {
+        var response = await _mediator.Send(request);
+        return response;
+    }
+
+    [HttpPut("End/{jobId}")]
+    public async Task<EndJobResponse> Put([FromRoute]string jobId, EndJobRequest request)
+    {
+        var response = await _mediator.Send(request);
+        return response;
+    }
+}
