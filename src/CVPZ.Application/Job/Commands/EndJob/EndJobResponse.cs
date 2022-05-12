cf69ab98fd21c67d37b0cc4f70006468f04579bc
Carter Barnes <carter.barnes@cremadevelopment.com>
2022-05-11 21:01:52 -0700

    Add command and update entity to end a job #912 #913

---
+++ src/CVPZ.Application/Job/Commands/EndJob/EndJobResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace CVPZ.Application.Job.Commands.EndJob
+{
+    public class EndJobResponse
+    {
+        public EndJobResponse(string jobId)
+        {
+            JobId = jobId;
+        }
+
+        public string JobId { get; }
+    }
+}
