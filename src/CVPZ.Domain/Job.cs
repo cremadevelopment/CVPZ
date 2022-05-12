90a621714cc0d28ff36d554609dad406856eab43
Carter Barnes <carter.barnes@cremadevelopment.com>
2022-05-11 19:28:13 -0700

    Add properties to the Job entity #909

--- src/CVPZ.Domain/Job.cs
+++ src/CVPZ.Domain/Job.cs
@@ -2,5 +2,9 @@
 
 public class Job : BaseEntity
 {
+    public string EmployerName { get; set; }
     public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTimeOffset StartDate { get; set; }
+    public DateTimeOffset? EndDate { get; set; }
 }
