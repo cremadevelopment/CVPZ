e499bcaa8d359a47e11f9287f878ef9a9f2646be
Carter Barnes {[/] <carterabarnes@gmail.com>
2022-05-28 08:38:18 -0700

    cleanup and update extensions with tags

--- src/CVPZ/Api/JobApiExtensions.cs
+++ src/CVPZ/Api/JobApiExtensions.cs
@@ -8,20 +8,23 @@ public static class JobApiExtensions
 {
     public static WebApplication MapJobApi(this WebApplication app)
     {
-
-        app.MapPost("CreateJob", async ([FromServices] IMediator mediator, CreateJob.Request request) => {
+        app.MapPost("/Job/Create", async ([FromServices] IMediator mediator, CreateJob.Request request) => {
             var result = await mediator.Send(request);
             return result.Match(
                 response => Results.Ok(response),
                 error => Results.BadRequest(error)
             );
-        }).Produces<CreateJob.Response>();
+        })
+        .Produces<CreateJob.Response>()
+        .WithTags("Job");
 
-        app.MapPost("EndJob", async ([FromServices] IMediator mediator, EndJob.Request request) =>
+        app.MapPost("Job/End", async ([FromServices] IMediator mediator, EndJob.Request request) =>
         {
             var response = await mediator.Send(request);
             return response;
-        }).Produces<EndJob.Response>();
+        })
+        .Produces<EndJob.Response>()
+        .WithTags("Job");
 
         return app;
     }
