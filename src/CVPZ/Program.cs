89ccb86d9a9b069414f40b18c14984ee83f6e4b4
Carter Barnes {[/] <carterabarnes@gmail.com>
2022-05-27 20:44:47 -0700

    Convert all endpoints to minimal api endpoints

--- src/CVPZ/Program.cs
+++ src/CVPZ/Program.cs
@@ -1,6 +1,10 @@
 using CVPZ.Application;
+using CVPZ.Application.Configuration.Queries.GetUserInfo;
+using CVPZ.Application.Job;
 using CVPZ.Core;
 using CVPZ.Infrastructure;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,17 +21,35 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => {
-    options.CustomSchemaIds(type => type.ToString());
+    options.CustomSchemaIds(type => type.ToString().Replace("+", ""));
 });
+//builder.Services.ConfigureSwaggerGen(options => options.CustomSchemaIds(x => x.FullName.Replace("+", "_")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+
+app.MapGet("GetUserInfo", async ([FromServices] IMediator mediator) => {
+    return await mediator.Send(new GetUserInfo());
+}).Produces<UserInfo>();
+
+app.MapPost("CreateJob", async ([FromServices] IMediator mediator, CreateJob.Request request) => {
+    var result = await mediator.Send(request);
+    return result.Match(
+        response => Results.Ok(response),
+        error => Results.BadRequest(error)
+    );
+}).Produces<CreateJob.Response>();
+
+app.MapPost("EndJob", async ([FromServices] IMediator mediator, EndJob.Request request) =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    var response = await mediator.Send(request);
+    return response;
+}).Produces<EndJob.Response>();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
