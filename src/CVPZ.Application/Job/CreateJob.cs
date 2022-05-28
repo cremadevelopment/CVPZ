78535a60e277a1e2b7581c747d0233dec203099d
Carter Barnes <carter.barnes@cremadevelopment.com>
2022-05-27 19:13:49 -0700

    Update job controller

--- src/CVPZ.Application/Job/CreateJob.cs
+++ src/CVPZ.Application/Job/CreateJob.cs
@@ -1,13 +1,19 @@
 ﻿using CVPZ.Application.Job.Events;
+using CVPZ.Core;
 using CVPZ.Infrastructure.Data;
 using MediatR;
+using OneOf;
 
 namespace CVPZ.Application.Job;
 
 public static class CreateJob
 {
+    public class Errors
+    {
+        public static Error JobTitleRequired => new(Code: nameof(JobTitleRequired), "Title is required");
+    }
 
-    public class Handler : IRequestHandler<Request, Response>
+    public class Handler : IRequestHandler<Request, OneOf<Response, Error>>
     {
         private readonly CVPZContext _context;
         private readonly IMediator _mediator;
@@ -18,8 +24,11 @@ public static class CreateJob
             this._mediator = mediator;
         }
 
-        public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+        public async Task<OneOf<Response, Error>> Handle(Request request, CancellationToken cancellationToken)
         {
+            if (string.IsNullOrWhiteSpace(request.Title))
+                return Errors.JobTitleRequired;
+
             var entity = await MapToEntity(request);
             await PersistEntity(entity);
 
@@ -58,7 +67,7 @@ public static class CreateJob
         string? Description,
         DateTimeOffset StartDate,
         DateTimeOffset? EndDate)
-        : IRequest<Response>;
+        : IRequest<OneOf<Response, Error>>;
 
     public record Response (
         string Id,
