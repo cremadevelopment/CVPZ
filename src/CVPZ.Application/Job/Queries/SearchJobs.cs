54376078ac312c0ac9f7efbc983f81be6796c22f
Carter Barnes <carter.barnes@cremadevelopment.com>
2022-06-02 07:37:46 -0700

    Add job search parameters and expressions for searching.

--- src/CVPZ.Application/Job/Queries/SearchJobs.cs
+++ src/CVPZ.Application/Job/Queries/SearchJobs.cs
@@ -2,12 +2,13 @@
 using CVPZ.Infrastructure.Data;
 using MediatR;
 using OneOf;
+using System.Linq.Expressions;
 
 namespace CVPZ.Application.Job.Queries;
 
 public static class SearchJobs
 {
-    public record Request(): IRequest<OneOf<Response, Error>>;
+    public record Request(string? Title, string? Employer): IRequest<OneOf<Response, Error>>;
     public record Response(IEnumerable<Job> Jobs);
     public record Job(string EmployerName, string Title, string? Description, DateTimeOffset StartDate, DateTimeOffset? EndDate);
 
@@ -23,10 +24,28 @@ public static class SearchJobs
         public async Task<OneOf<Response, Error>> Handle(Request request, CancellationToken cancellationToken)
         {
             var jobResults = _context.Jobs
+                    .Where(SearchTitle(request.Title))
+                    .Where(SearchEmployer(request.Employer))
                     .Select(x => new Job(
                         x.EmployerName, x.Title, x.Description,
                         x.StartDate, x.EndDate));
             return new Response(jobResults);
+        }
+
+        private Expression<Func<Domain.Job, bool>> SearchTitle(string? title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                return x => true;
+
+            return x => x.Title.ToLower().Contains(title.ToLower());
+        }
+
+        private Expression<Func<Domain.Job, bool>> SearchEmployer(string? employer)
+        {
+            if (string.IsNullOrWhiteSpace(employer))
+                return x => true;
+
+            return x => x.EmployerName.ToLower().Contains(employer.ToLower());
         }
     }
 }
