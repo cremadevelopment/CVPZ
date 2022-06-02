9d35f9bd952e91661fee707644975f327bc92a5a
Carter Barnes <carter.barnes@cremadevelopment.com>
2022-06-01 21:25:00 -0700

    Add get jobs

---
+++ src/CVPZ.Application/Job/Queries/GetJobs.cs
@@ -0,0 +1,32 @@
+﻿using CVPZ.Core;
+using CVPZ.Infrastructure.Data;
+using MediatR;
+using OneOf;
+
+namespace CVPZ.Application.Job.Queries;
+
+public static class GetJobs
+{
+    public record Request(): IRequest<OneOf<Response, Error>>;
+    public record Response(IEnumerable<Job> Jobs);
+    public record Job(string EmployerName, string Title, string? Description, DateTimeOffset StartDate, DateTimeOffset? EndDate);
+
+    public class Handler : IRequestHandler<Request, OneOf<Response, Error>>
+    {
+        private readonly CVPZContext _context;
+
+        public Handler(CVPZContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<OneOf<Response, Error>> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var jobResults = _context.Jobs
+                    .Select(x => new Job(
+                        x.EmployerName, x.Title, x.Description,
+                        x.StartDate, x.EndDate));
+            return new Response(jobResults);
+        }
+    }
+}
