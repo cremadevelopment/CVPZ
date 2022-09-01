b9ece623f1ca53cc3bb9e97cfee0805ef3fc0c50
Carter Barnes {[/] <carter.barnes@cremadevelopment.com>
2022-08-31 21:24:51 -0700

    Template navigation UI along with initial setup of auth config

--- src/CVPZ/ClientApp/src/app/app.component.ts
+++ src/CVPZ/ClientApp/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +12,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public pingResult?: PingResponse;
 
-  constructor(http: HttpClient) {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(http: HttpClient, private breakpointObserver: BreakpointObserver) {
     http.get<PingResponse>('/system/ping').subscribe(result => {
       this.pingResult = result;
     }, error => console.error(error));
--- src/CVPZ/ClientApp/src/app/app.module.ts
+++ src/CVPZ/ClientApp/src/app/app.module.ts
@@ -5,14 +5,68 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+import { MsalGuard, MsalGuardConfiguration, MsalBroadcastService, MsalModule, MsalService, MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalRedirectComponent } from '@azure/msal-angular';
+import { InteractionType, IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
+
+import { CvpzNavigationComponent } from './cvpz-navigation/cvpz-navigation.component';
+import { msalConfig } from './auth-config';
+
+/**
+ * Here we pass the configuration parameters to create an MSAL instance.
+ * For more info, visit: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-angular/docs/v2-docs/configuration.md
+ */
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication(msalConfig);
+}
+
+/**
+ * Set your default interaction type for MSALGuard here. If you have any
+ * additional scopes you want the user to consent upon login, add them here as well.
+ */
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return {
+    interactionType: InteractionType.Redirect,
+  };
+}
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvpzNavigationComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, BrowserAnimationsModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MsalModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService],
+  bootstrap: [AppComponent, MsalRedirectComponent]
 })
 export class AppModule { }
