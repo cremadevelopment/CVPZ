b0d227ad1b594e7bfb3c37a3f9ce6c164b479b78
Carter Barnes {[/] <carter.barnes@cremadevelopment.com>
2022-09-07 19:41:45 -0700

    Add router and default route to home component

---
+++ src/CVPZ/ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+];
+
+const isIframe = window !== window.parent && !window.opener;
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
