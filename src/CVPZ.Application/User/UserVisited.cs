cabbcb5899171c1cc7a491d6833b08eb3c581ee0
Carter Barnes {[/] <carter.barnes@cremadevelopment.com>
2022-11-02 21:16:54 -0700

    Add UserVisited event with database User update

---
+++ src/CVPZ.Application/User/UserVisited.cs
@@ -0,0 +1,38 @@
+﻿using CVPZ.Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace CVPZ.Application.User;
+
+public class UserVisited
+{
+    public record Event ( ClaimsPrincipal principal ) : INotification;
+
+    public class Listener : INotificationHandler<Event>
+    {
+        private readonly CVPZContext _context;
+
+        public Listener(CVPZContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task Handle(Event notification, CancellationToken cancellationToken)
+        {
+            var objectId = notification.principal.Claims.Single(c => c.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+            
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.ObjectId == objectId);
+            if (null == user)
+            {
+                user = new Domain.User
+                {
+                    NickName = notification.principal.Claims.Single(c => c.Type == "name").Value,
+                    ObjectId = objectId
+                };
+                await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
